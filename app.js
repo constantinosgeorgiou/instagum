b7618c7245b927cf35318f582c35897fbbfeb116
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2018-09-28 18:54:44 +0300

    Added multiple routes, connected mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var express = require("express"),
-    app     = express();
+// configure dotenv
+require('dotenv').load();
 
-app.get("/", function(req,res){
-    res.render("index.ejs");
-})
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("\n server as started \n");
-});
+// Database config
+const databaseUri = process.env.MONGODB_URI;
+mongoose.connect(databaseUri)
+    .then(() => console.log(`Database connected`))
+    .catch(err => console.log(`Database connection error: ${err.message}`));
+
+
+app.set("view engine", "ejs");
+
+
+// Requiring routes
+const commentRoutes = require("./routes/comments"),
+      postRoutes    = require("./routes/posts"),
+      indexRoutes   = require("./routes/index"),
+      userRoutes    = require("./routes/users");
+
+// Using routes
+app.use("/", indexRoutes);
+app.use("/posts", postRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/users", userRoutes);
+
+// Where the app is available
+
+
+app.listen(process.env.PORT, process.env.IP, () => console.log(`\nApp listening on port ${process.env.PORT}!\n`));
